1333ceec566cb6ac4d0253414e8b124dd35d7b2e
Lorenzo Yanga <lorenzoyanga@gmail.com>
2025-05-31 11:25:49 +0800

    testing

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lyanga <lyanga@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 03:04:41 by lyanga            #+#    #+#             */
-/*   Updated: 2025/05/10 20:03:43 by lyanga           ###   ########.fr       */
+/*   Updated: 2025/05/31 11:25:40 by lyanga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,5 +80,7 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void*));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+
+// adding extras here
 
 #endif
